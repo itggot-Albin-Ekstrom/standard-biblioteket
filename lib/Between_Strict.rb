4910099a5ff075cfba4fca1f070649d029694bef
itggot-Albin-Ekstrom <albin.ekstrom@itggot.se>
2018-05-02 13:06:16 +0200

    jag gjorde between_Strict

---
+++ lib/Between_Strict.rb
@@ -0,0 +1,23 @@
+# Public: bestämer om de första numret ligger precis i mellan de andra numrerna
+#
+# number1 - the INteger to check if it is precisly between the other two numbers 
+# number2 - The Integer that is the lowest.
+# number3 - The INteger that is the highest 
+#
+# Examples
+#
+#   between(0,-1,1)
+#   # => true
+#
+# Returns true or false 
+def Between_strict(number1,number2,number3)
+    output = false
+    if number1 > number2 && number1 < number3
+        if number1 != number2 && number3 != number2
+            output = true 
+        end
+    end
+    return output
+end
+
+        
