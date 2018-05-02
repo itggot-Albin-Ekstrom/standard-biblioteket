f1362c2fd149f2ec50e9d13746232059492cf960
itggot-Albin-Ekstrom <albin.ekstrom@itggot.se>
2018-05-02 13:50:52 +0200

    Jag har gjort Max_of_Two

---
+++ lib/Max_of_Two.rb
@@ -0,0 +1,21 @@
+# Public: Decides the higest number 
+#
+# number1 - The Integer that is the first value  
+# number2 - The Integer that is the second value
+# 
+#
+# Examples
+#
+#   Max_of_Two(0,1)
+#   # => 1
+#
+# Returns the higest number
+def Max_of_Two(number1,number2)
+    if number1 < number2
+        output = number2
+    else
+         output = number1
+    end
+    return output
+end
+
